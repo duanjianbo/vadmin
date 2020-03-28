f9f5c0f910a2ca11e3af5db0400a65d3e59dd944
DuanJianbo <jianbo.duan@foxmail.com>
2020-03-28 18:39:27 +0800

    vadmin目录部署

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,16 @@
 module.exports = {
   runtimeCompiler: true,
+
   css: {
     sourceMap: true
   },
+
   chainWebpack: (config) => {
     config.optimization.minimizer('terser').tap((args) => {
       args[0].terserOptions.compress.drop_console = true
       return args
     })
-  }
+  },
+
+  publicPath: '/vadmin'
 }
